bd17c08b1598618180618db636c7b06412a80acb
Chris Arnesen <chris.arnesen@gmail.com>
2020-08-05 23:57:07 -0700

    Add TSDoc strings to source code
    Update dev dependencies
    
    Use tabs instead of spaces
    
    Bump minor version to 0.2.0

---
+++ changelog.md
@@ -0,0 +1,7 @@
+# **@carnesen/coded-error** changelog
+
+## 0.2.0 (2020-08-05)
+
+### Features
+
+- Added TSDoc comments to source code
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
+/**
+ * An `Error` class with additional properties "code" and "data"
+ */
 export class CodedError extends Error {
-  public readonly code?: any;
-  public readonly data?: any;
-  constructor(message?: string, code?: any, data?: any) {
-    super(message);
-    if (typeof code !== 'undefined') {
-      this.code = code;
-    }
-    if (typeof data !== 'undefined') {
-      this.data = data;
-    }
-    if (typeof this.stack === 'string') {
-      this.stack = `${this.stack}\n`;
-    }
-    Object.setPrototypeOf(this, new.target.prototype);
-  }
+	public readonly code?: any;
+
+	public readonly data?: any;
+
+	/**
+	 * @param message An error message
+	 * @param code An error code, typically a string or number
+	 * @param data Arbitrary data to attach to the object
+	 */
+	public constructor(message?: string, code?: any, data?: any) {
+		super(message);
+		if (typeof code !== 'undefined') {
+			this.code = code;
+		}
+		if (typeof data !== 'undefined') {
+			this.data = data;
+		}
+		if (typeof this.stack === 'string') {
+			this.stack = `${this.stack}\n`;
+		}
+		Object.setPrototypeOf(this, new.target.prototype);
+	}
 }
