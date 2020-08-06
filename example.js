7352bdb8bd25c4dc678d673f4f4c5ef7fa7554b9
Chris Arnesen <chris.arnesen@gmail.com>
2020-08-05 23:57:07 -0700

    Drop support for Node.js 10
    Add TSDoc strings to source code
    
    Update dev dependencies
    
    Use tabs instead of spaces
    
    Bump minor version to 0.2.0

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 const { CodedError } = require('.');
 
-const err = new CodedError('An error has occurred', 127, { foo: 'bar'});
+const err = new CodedError('An error has occurred', 127, { foo: 'bar' });
 
 console.log(err);
